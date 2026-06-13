1bf7ab42ed07257911e82e934866bc8104ec2c65
Anzyll <anzil2003m@gmail.com>
2026-06-13 10:47:18 +0530

    fix: prevent overlap slot template creation

--- server/src/main/java/com/bookmyvenue/server/venue/controller/VenueController.java
+++ server/src/main/java/com/bookmyvenue/server/venue/controller/VenueController.java
@@ -10,7 +10,6 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -20,6 +19,7 @@ public class VenueController {
     private final VenueService venueService;
 
     @PostMapping("/vendor/venues")
+    @PreAuthorize("hasRole('VENDOR')")
     @ResponseStatus(HttpStatus.CREATED)
     public VenueResponse createVenue(
             @RequestBody CreateVenueRequest request
@@ -27,20 +27,21 @@ public class VenueController {
         return venueService.createVenue(request);
     }
 
-    @GetMapping("vendor/venues")
-    @PreAuthorize("hasRole('USER')")
+    @GetMapping("/vendor/venues")
+    @PreAuthorize("hasRole('VENDOR')")
     @ResponseStatus(HttpStatus.OK)
     public List<VenueResponse> getAllVenues(){
         return venueService.getAllVenues();
     }
 
-    @GetMapping("vendor/venues/{venueId}")
+    @GetMapping("/vendor/venues/{venueId}")
     @ResponseStatus(HttpStatus.OK)
+    @PreAuthorize("hasRole('VENDOR')")
     public VenueResponse getVenue(@PathVariable Long venueId){
         return venueService.getVenue(venueId);
     }
 
-    @GetMapping("venues")
+    @GetMapping("/venues")
     @ResponseStatus(HttpStatus.OK)
     public List<VenueResponse> getApprovedVenues(
                                                    @RequestParam(required = false) String search,
@@ -49,7 +50,7 @@ public class VenueController {
         return venueService.getApprovedVenues(search,district,categoryId);
     }
 
-    @GetMapping("venues/{id}")
+    @GetMapping("/venues/{id}")
     @ResponseStatus(HttpStatus.OK)
     public VenueResponse getApprovedVenue(
             @PathVariable Long id
@@ -57,7 +58,8 @@ public class VenueController {
         return venueService.getApprovedVenue(id);
     }
 
-    @PatchMapping("venues/{id}")
+    @PatchMapping("/venues/{id}")
+    @PreAuthorize("hasRole('VENDOR')")
     @ResponseStatus(HttpStatus.OK)
     public VenueResponse updateVenue(
             @PathVariable Long id,
@@ -66,7 +68,8 @@ public class VenueController {
         return venueService.updateVenue(id, request);
     }
 
-    @DeleteMapping("venues/{id}")
+    @DeleteMapping("/venues/{id}")
+    @PreAuthorize("hasRole('VENDOR')")
     @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteVenue(
             @PathVariable Long id
